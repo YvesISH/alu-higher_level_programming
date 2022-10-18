6d50b9ad19ade85859251a948d7be0d1db0c19d6
YvesISH <y.ishimwe2@alustudent.com>
2022-10-17 17:52:06 -0700

    Mandatory Task

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:-1]
+print(f"first 3 letters: {word_first_3}")
+print(f"last 2 letters: {word_last_2}")
+print(f"middle word: {middle_word}")
