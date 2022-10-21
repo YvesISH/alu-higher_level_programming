67a1f229c53257a0dc1f29649d573a8e8a6ba9e5
YvesISH <y.ishimwe2@alustudent.com>
2022-10-21 03:59:06 -0700

    Mandatory Task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for n in range(ord('a'), ord('z') + 1):
-    if n != ord('e') and n != ord('q'):
-        print("{:c}".format(n), end="")
+for alphabet in range(ord('a'), ord('z') + 1):
+    if alphabet != ord('e') and alphabet != ord('q'):
+        print("{:c}".format(alphabet), end="")
