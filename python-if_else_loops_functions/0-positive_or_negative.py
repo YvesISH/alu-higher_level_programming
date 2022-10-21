fd121355c210993b4dd25530a732d3b812a92564
YvesISH <y.ishimwe2@alustudent.com>
2022-10-20 22:53:59 -0700

    Mandatory Task

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randit(-10, 10)
 if (number > 0):
-    print('is positive')
+    print(f"{number} is positive")
 elif (number == 0):
-    print('is zero')
+    print(f"{number} is zero")
 else:
-    print('is negative')
+    print(f"{number} is negative")
