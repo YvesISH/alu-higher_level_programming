34f16f42497faa99673f9c9b7124ee9920f3d5fc
YvesISH <y.ishimwe2@alustudent.com>
2022-10-21 05:03:09 -0700

    Mandatory Task

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for chr in sr:
+        if ord(chr) >= ord('a') and ord(chr) <= ord('z'):
+            char = character(ord(chr) - 32)
+        else:
+            char = chr
+            print("{:s}".format(char), end="")
+        print('')
