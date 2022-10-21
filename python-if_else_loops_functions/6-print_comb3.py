d167ce01c9e7b9f76156ce1bdb90515b173bcc7c
YvesISH <y.ishimwe2@alustudent.com>
2022-10-21 04:37:52 -0700

    Mandatory Task

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 9):
+    for numbers in range(number + 1, 10):
+        if number == 8:
+            print("{}{}".format(number, numbers))
+        else:
+            print("{}{}".format(number, numbers)end=", ")
