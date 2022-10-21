d682631ebdb18db386b16ff230d9ed587701a81b
YvesISH <y.ishimwe2@alustudent.com>
2022-10-21 03:49:52 -0700

    Mandatory Task

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(x), end="")
+for alphabet in range(ord('a'), ord('z') + 1):
+        print("{:c}".format(alphabet), end="")
