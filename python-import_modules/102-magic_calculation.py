233e17016e82d2ac6b5e4fe17a6af069d934158f
YvesISH <y.ishimwe2@alustudent.com>
2022-10-24 04:49:23 -0700

    Advanced Task

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
-        else:
-            return sub(a, b)
+        return c
+    else:
+        return sub(a, b)
