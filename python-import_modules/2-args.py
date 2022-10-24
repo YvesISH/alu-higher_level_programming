737ec74e27066856e891053f1161e64e16f95bac
YvesISH <y.ishimwe2@alustudent.com>
2022-10-24 03:31:48 -0700

    Mandatory Task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(argv_count))
     elif argv_count == 1:
         print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.srgv[1]))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
         print("{:d} arguments:".format(argv_count))
         while index <= argv_count:
