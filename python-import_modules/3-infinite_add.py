87feed4c5b04aef9b9fd95e8c4819cfdc79f93a3
YvesISH <y.ishimwe2@alustudent.com>
2022-10-24 03:40:45 -0700

    Mandatory Task

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin.python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    sum = 0
+    while index <= argv_count:
+        sum += int(sys.argv[index])
+        index += 1
+    print(sum)
