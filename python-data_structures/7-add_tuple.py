9b33ec805daf625a55918fa0b267feb19e7d6a44
YvesISH <y.ishimwe2@alustudent.com>
2022-10-31 02:18:52 -0700

    Mandatory TASK

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a, b = len(tuple_a), len(tuple_b)
     tup = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
-                  (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+                (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
     return tup
