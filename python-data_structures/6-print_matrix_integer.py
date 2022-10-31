c791557619c3128012431c2bfce1a583c7b5b16d
YvesISH <y.ishimwe2@alustudent.com>
2022-10-31 01:54:07 -0700

    Mandatory TASK

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
-        print("".join("{:d}".format(i) for i in element))
+        print(" ".join("{:d}".format(i) for i in element))
