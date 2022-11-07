1feab98d5559f01c7f0d8ec7883cf08e33a4d2ec
YvesISH <y.ishimwe2@alustudent.com>
2022-11-07 06:36:58 -0800

    Mandatory Task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Define class Square."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new Square.
         Args:
-        size (int): The size of the new square.
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0)
+            raise ValueError("size must be >= 0")
         self.__size = size
