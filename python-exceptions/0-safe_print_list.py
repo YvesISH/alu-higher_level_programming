544feba3922f241420aae4968f25e65238958578
YvesISH <y.ishimwe2@alustudent.com>
2022-11-07 01:27:33 -0800

    Mandatory Task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     start = 0
-    for start in range(x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             start += 1
