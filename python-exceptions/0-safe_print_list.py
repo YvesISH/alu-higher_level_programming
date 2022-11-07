1710d6bbe35ee150371e3cef280c82dc0ce191c0
YvesISH <y.ishimwe2@alustudent.com>
2022-11-07 01:29:15 -0800

    Mandatory Task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             start += 1
         except IndexError:
             break
-        print("")
-        return (start)
+    print("")
+    return (start)
