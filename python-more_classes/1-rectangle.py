7477f99bf4d119561a735b8e76bf0f2fb6e5879f
YvesISH <y.ishimwe2@alustudent.com>
2022-11-10 07:43:05 -0800

    Mandatory Task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
