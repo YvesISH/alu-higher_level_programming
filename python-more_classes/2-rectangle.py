b290e625afd18bfc21d36fd3906c2b7edf08131b
YvesISH <y.ishimwe2@alustudent.com>
2022-11-10 07:58:23 -0800

    Mandatory Task

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Rectangle:
+    """Define aa class Rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initializing
+            Args: height & width
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance (value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set Height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance (value, int):
+            raise TypeError("heigh must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of a Rectangle"""
+        return (self.__width * self.__height)
+
+    def perimiter(self):
+        """Return Perimiter of the Rectangle"""
+        if self.__width or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
