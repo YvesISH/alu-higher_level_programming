465ed1832d134889eda943cd832ed4e74cf204d2
YvesISH <y.ishimwe2@alustudent.com>
2022-11-10 08:16:42 -0800

    Mandatory Task

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -63,6 +63,7 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
