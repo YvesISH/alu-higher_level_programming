e0cbcc8beae2ad3f1b74b05fc1fb4754a3e7879f
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 01:00:14 -0800

    Mandatory Task

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defining a class."""
+
+
+class BaseGeometry:
+    """represents a geometry."""
+
+    def area(self):
+        """raise an exception error message."""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """validate value."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
