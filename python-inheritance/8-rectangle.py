1ba7c0aae597487b4f9c0531246e496495069f72
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 02:40:31 -0800

    Mandatory Task.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """A rectangle class that inhert from Geometry"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
-    """represent a rectangle
-    """
+    """represent a rectangle """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
-        self.__height = 
+        self.__height = height
