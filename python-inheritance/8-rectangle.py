b2af54561c70252d153c4600ab461ab556173e14
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 02:36:32 -0800

    Mandatory Task.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""A rectangle class that inhert from Geometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """represent a rectangle
-    """
+    """A representation of a rectangle"""
     def __init__(self, width, height):
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
