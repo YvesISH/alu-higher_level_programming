377a8968ce14326ed90820d6819f08ccb8015e23
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 00:24:02 -0800

    Mandatory Task

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A Boolean Function"""
+
+
+def is_same_class(obj, a_class):
+    """Check if object is instance of a class."""
+    return True if type(obj) == a_class else False
