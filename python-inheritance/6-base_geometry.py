04f771bf965d78e02cae9e1a7d8ba66bcfe07be3
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 00:52:16 -0800

    Mandatory Task

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """raise an exception message."""
-        raise Exception("area() is not implemented.")
+        raise Exception("area() is not implemented")
