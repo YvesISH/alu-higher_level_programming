c6b5b57c71129fc9fa563afa2b75ccfc14477178
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 00:50:33 -0800

    Mandatory Task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining BaseGeometry Class."""
+
+
+class BaseGeometry:
+    """represent a geometry."""
+
+    def area(self):
+        """raise an exception message."""
+        raise Exception("area() is not implemented.")
