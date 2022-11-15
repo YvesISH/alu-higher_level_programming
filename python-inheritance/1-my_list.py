abe20f418c2ac1e1bb4410b18d5ba18640232c8f
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 00:14:37 -0800

    Mandatory Task

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,8 +4,9 @@
 
 class Mylist(list):
     """A class that inherits from parent class list."""
+
     def print_sorted(self):
-        '''
+        """
         prints the list, but sorted
-        '''
+        """
         print(sorted(self))
