9fc54a74312da057bced4ea52497e5ac11334ee0
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 01:44:12 -0800

    Mandatory Task.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
-"""A rectangle class that inherits from geometry."""
-
-
-class BaseGeometry:
-    """represents base Geometry."""
-
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validate a parameter as an integer."""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""A rectangle class that inhert from Geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle."""
+    """represent a rectangle
+    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
-        self.integer_validator("heigth", height)
         self.__height = height
