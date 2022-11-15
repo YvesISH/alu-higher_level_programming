006c4e30ed850783939b33815ea90d0ead56bfec
YvesISH <y.ishimwe2@alustudent.com>
2022-11-15 00:33:52 -0800

    Mandatory Task

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#1/usr/bin/python3
+"""Function that check if Object is instance of Class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if Obj is a kind of a class Otherwise False."""
+
+    return isinstance(obj, a_class)
