491cb0dcb8eb01d9d871233e6651891486772f83
YvesISH <y.ishimwe2@alustudent.com>
2022-11-16 14:05:28 -0800

    Advanced Task.

---
+++ python-input_output/100-append.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string."""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
