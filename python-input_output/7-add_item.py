dcfce04c238ab4a803ca8e0a5d4d06be00f845ef
YvesISH <y.ishimwe2@alustudent.com>
2022-11-16 07:22:33 -0800

    Mandatory Task.

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Adda ll arguments to a python list."""
+
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+all_list = [args for args in sys.argv[1:]]
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+items.extend(all_list)
+save_to_json_file(items, "add_item.json")
