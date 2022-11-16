29cd26d5d704b3b4a5e880872164220d3a45456e
YvesISH <y.ishimwe2@alustudent.com>
2022-11-16 07:24:54 -0800

    Mandatory Task.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Adda ll arguments to a python list."""
+"""add all arguments to a Python list, and then save them to a file."""
 
 
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
-all_list = [args for args in sys.argv[1:]]
+all_list = [arg for arg in sys.argv[1:]]
 try:
     items = load_from_json_file("add_item.json")
 except FileNotFoundError:
