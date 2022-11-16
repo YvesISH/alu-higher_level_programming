782505daf20d0bcd17a5b40e0975cb9e189e9b6c
YvesISH <y.ishimwe2@alustudent.com>
2022-11-16 07:43:39 -0800

    MAndatory Task.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """Add all argument to a python list."""
 
-
 import sys
-
 
 if __name__ = "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        loadFile = load_form_json_file("add_item.json")
+        items = load_form_json_file("add_item.json")
     except FileNotFoundError:
-        loadFile = []
-
-    argc = len(sys.argv)
-    for idx in range(1, argc):
-        loadFile.append(sys.argv[idx])
-    save_to_json_file(loadFile, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_items.json")
