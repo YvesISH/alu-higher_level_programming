c9d12be7bb123fbd68f60afb50c50d16a69088a0
YvesISH <y.ishimwe2@alustudent.com>
2022-11-22 10:56:41 -0800

    Mandatory TASK.

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that takes GitHub credentials."""
+
+
+if __name__ == '__main__':
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+
+    url = 'https://api.github.com/useer'
+    username = sys.argv[1]
+    token = sys.argv[2]
+    auth = HTTPBasicAuth(username=username, password=token)
+    response = requests.get(url, auth=auth)
+    result = response.json()
+    print(result.get('id'))
