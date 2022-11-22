40d17a9fabb3d071c433c364fd64702739f42c9d
YvesISH <y.ishimwe2@alustudent.com>
2022-11-22 07:02:14 -0800

    Mandatory Task.

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""python script that fetches url link."""
+"""Documented"""
 
 
 import urllib.request
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        yy = response.read()
-        print("response.")
-        print("\t- type: {}".format(type(yy)))
-        print("\t- content: {}".format(yy))
-        print("\t- utf8 content: {}".format(yy.decode('utf-8')))
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
