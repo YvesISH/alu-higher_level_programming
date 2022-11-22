8f5c20db4ed11f336ea072a61d604600a7f5d9f8
YvesISH <y.ishimwe2@alustudent.com>
2022-11-22 11:03:36 -0800

    Mandatory TASK.

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     user = sys.argv[1]
     xyz = sys.argv[2]
     authori = HTTPBasicAuth(username=user, password=xyz)
-    response = requests.get(url, authori=authori)
+    response = requests.get(url, auth=authori)
     result = response.json()
     print(result.get('id'))
