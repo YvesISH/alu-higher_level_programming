a8515eefe2da1dae7585ba5c724c28b840631038
YvesISH <y.ishimwe2@alustudent.com>
2022-11-22 07:35:20 -0800

    Mandatory Task.

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that sends POST request."""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+    data = urlencode({"email": sys.argv[2]
+        }).encode("ascii")
+
+    request = Request(sys.srgv[1], data)
+    with urlopen(request) as re:
+        print(re.read().decode('utf-8'))
