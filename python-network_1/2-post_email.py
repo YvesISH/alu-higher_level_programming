f6db06daad8181707d4c31f35ba5a647bef7c2af
YvesISH <y.ishimwe2@alustudent.com>
2022-11-22 07:42:46 -0800

    Mandatory Task.

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     import urllib.request
     import urllib.parse
     import sys
-    data = urlencode({"email": sys.argv[2]
-        }).encode("ascii")
+    data = urlencode({"email": sys.argv[2]\
+            }).encode("ascii")
 
     request = Request(sys.srgv[1], data)
     with urlopen(request) as re:
