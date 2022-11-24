ddba29efb78f4e06d24038f184abf41f99b76037
YvesISH <y.ishimwe2@alustudent.com>
2022-11-24 04:50:41 -0800

    Create Table.

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+--Script that create database and the table.
+CREATE DATABASE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
