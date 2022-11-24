39e1b1373e979953019d92c68c66dcfb7a49523a
YvesISH <y.ishimwe2@alustudent.com>
2022-11-24 04:58:46 -0800

    Table & DB

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
---Script that create database and the table.
-CREATE DATABASE hbtn_0d_usa
+-- creates the database and the table.
+CREATE  DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL);
