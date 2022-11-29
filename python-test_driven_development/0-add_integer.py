3714ecc1e23696217337dc579c62deaaba1d71fb
YvesISH <y.ishimwe2@alustudent.com>
2022-11-29 12:54:27 -0800

    Test Driven Development.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Module with add function.
-"""
+"""Module with add function."""
 
 
 def add_integer(a, b=98):
-    """
-    Return integer addition of a and b
-    """
+   """ Return integer addition of a and b."""
     if type(a) is not int and type(b) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
