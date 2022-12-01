0b4fabc1482f0a1fb23b00b097c139afdcc5d04f
YvesISH <y.ishimwe2@alustudent.com>
2022-12-01 05:10:18 -0800

    Test Driven Development.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-This "2-matrix_divided" module supplies one function,
-matrix_divided(matrix, div).
+This is a Matrix Divided function
 """
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
+    """Divides all elements in a matrix"""
     if type(matrix) is not list:
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+            "matrix must be a matrix of integers/floats")
     size = None
-    for l in matrix:
-        if type(l) is not list:
+    for yy in matrix:
+        if type(yy) is not list:
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(yy)
+        elif size != len(yy):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
+        for i in yy:
             if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
@@ -27,4 +26,4 @@ integers/floats")
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    return [[round(i / div, 2) for i in yy] for yy in matrix]
