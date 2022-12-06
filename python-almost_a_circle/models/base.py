c7debb07bdd5f71856fdc20180123571d4eb58ef
YvesISH <y.ishimwe2@alustudent.com>
2022-12-06 06:09:58 -0800

    Base Class.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """this is a base class."""
+import json
+import csv
+import turtle
 
 
 class Base:
@@ -11,5 +14,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_object += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
