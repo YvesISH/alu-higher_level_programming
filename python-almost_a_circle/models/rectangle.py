49f196bb4370ad7b1e73c8615b6355a9ab9769fe
YvesISH <y.ishimwe2@alustudent.com>
2022-12-06 07:06:26 -0800

    Rectangle Class.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,5 +91,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns [Rectangle]"""
-        return "[Rectabgle] ({}) {}/{} - {}/{}" \
-                .format(self.id, self.x, self.y, self.width, self.height)
+         return "[Rectangle] ({}) {}/{} - {}/{}" \
+            .format(self.id, self.x, self.y, self.width, self.height)
