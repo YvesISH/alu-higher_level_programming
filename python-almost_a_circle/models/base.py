50cd82f6eee8e6c16262bbf8fae0dd27be4b98ff
YvesISH <y.ishimwe2@alustudent.com>
2022-12-06 08:57:16 -0800

    Base (:

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,3 +16,37 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Json to string."""
+        if list_dictionaries is None:
+            return "[]"
+
+        if len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """SaVe to a file."""
+        filename = cls.__name + ".json"
+        yy = []
+        if list_objs is not None:
+            for xx in list_objs:
+                yy.append(cls.to_dictionary(xx))
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(yy))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Json conversion."""
+        if json_string is None:
+            return []
+
+        if len(json_string) == 0:
+            return []
+
+        json_list = json.loads(json_string)
+        return json_list
