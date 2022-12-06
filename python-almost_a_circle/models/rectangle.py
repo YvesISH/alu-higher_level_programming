8d76278680d0bb0b46da31ad39a9cc4295716f7f
YvesISH <y.ishimwe2@alustudent.com>
2022-12-06 07:22:27 -0800

    Reactangle Class.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Rectangle class that inherits from Base."""
+"""Rectangle Instance classes"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Define Rectangle class."""
+    """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializing method."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,12 +16,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Property Decorator."""
+        """Getting the Width"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter Decorator."""
+        """Setting the width"""
+
         if type(value) != int:
             raise TypeError("width must be an integer")
 
@@ -32,12 +33,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Property Decorator."""
+        """height of a rectangle"""
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter property."""
+        """Height"""
 
         if type(value) != int:
             raise TypeError("height must be an integer")
@@ -49,12 +51,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """property decorator."""
+        """x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """setter Decorator."""
+        """x"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -63,12 +65,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Property decorator."""
+        """return y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter Decorator."""
+        """Set the value of y"""
 
         if type(value) != int:
             raise TypeError("y must be an integer")
@@ -79,11 +81,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """Area of a rectangle."""
+        """return the area of the rectangle"""
         return self.width * self.height
 
     def display(self):
-        """Display Function."""
+        """return  #"""
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -91,6 +93,35 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns [Rectangle]"""
-         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, 
-                                                        self.x, self.y, 
-                                                        self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}" \
+            .format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """argument"""
+        if len(args) != 0:
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[2]
+                self.x = args[3]
+                self.y = args[4]
+            except IndexError:
+                pass
+        elif len(kwargs) != 0:
+            self.id = kwargs["id"] if "id" in kwargs else self.id
+            self.width = kwargs["width"] if "width" in kwargs \
+                else self.width
+            self.height = kwargs["height"] if "height" in kwargs \
+                else self.height
+            self.x = kwargs["x"] if "x" in kwargs else self.x
+            self.y = kwargs["y"] if "y" in kwargs else self.y
+
+    def to_dictionary(self):
+        """dictionary representation"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
