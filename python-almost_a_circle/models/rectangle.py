766544ab3cd393a7a64866918d07f623ecfcecb0
YvesISH <y.ishimwe2@alustudent.com>
2022-12-06 06:47:31 -0800

    Rectangle Class.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,8 +9,71 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializing method."""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
+
+    @property
+    def width(self):
+        """Property Decorator."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter Decorator."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property Decorator."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter property."""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """property decorator."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter Decorator."""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Property decorator."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter Decorator."""
+
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
