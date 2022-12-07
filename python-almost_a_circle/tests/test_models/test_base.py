ce2a639fc9d87853ed085ca7c7c9bbbdf1ccc816
YvesISH <y.ishimwe2@alustudent.com>
2022-12-07 10:33:48 -0800

    (:

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-# test_base.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines unittests for base.py.
-
-Unittest classes:
-    TestBase_instantiation - line 23
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
+# Tests for 'base.py'
+"""
+    Define unittests for 'base.py'
+    Unittest classes:
+        TestBase_instantiation
+        TestBase_to_json_string
+        TestBase_save_to_file
+        TestBase_from_json_string
+        TestBase_create
+        TestBase_load_from_file
+        TestBase_save_to_file_csv
+        TestBase_load_from_file_csv
 """
 import os
 import unittest
@@ -21,7 +20,9 @@ from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+    """
+        Tests for testing instantiation of the Base class.
+    """
 
     def test_no_arg(self):
         b1 = Base()
@@ -108,7 +109,9 @@ class TestBase_instantiation(unittest.TestCase):
 
 
 class TestBase_to_json_string(unittest.TestCase):
-    """Unittests for testing to_json_string method of Base class."""
+    """
+        Tests for testing to_json_string method of Base class.
+    """
 
     def test_to_json_string_rectangle_type(self):
         r = Rectangle(10, 7, 2, 8, 6)
@@ -154,7 +157,9 @@ class TestBase_to_json_string(unittest.TestCase):
 
 
 class TestBase_save_to_file(unittest.TestCase):
-    """Unittests for testing save_to_file method of Base class."""
+    """
+        Tests for testing save_to_file method of Base class.
+    """
 
     @classmethod
     def tearDown(self):
@@ -232,7 +237,9 @@ class TestBase_save_to_file(unittest.TestCase):
 
 
 class TestBase_from_json_string(unittest.TestCase):
-    """Unittests for testing from_json_string method of Base class."""
+    """
+        Tests for testing from_json_string method of Base class.
+    """
 
     def test_from_json_string_type(self):
         list_input = [{"id": 89, "width": 10, "height": 4}]
@@ -286,7 +293,9 @@ class TestBase_from_json_string(unittest.TestCase):
 
 
 class TestBase_create(unittest.TestCase):
-    """Unittests for testing create method of Base class."""
+    """
+        Tests for testing create method of Base class.
+    """
 
     def test_create_rectangle_original(self):
         r1 = Rectangle(3, 5, 1, 2, 7)
@@ -338,11 +347,15 @@ class TestBase_create(unittest.TestCase):
 
 
 class TestBase_load_from_file(unittest.TestCase):
-    """Unittests for testing load_from_file_method of Base class."""
+    """
+        Tests for testing load_from_file_method of Base class.
+    """
 
     @classmethod
     def tearDown(self):
-        """Delete any created files."""
+        """
+            Delete any created files.
+        """
         try:
             os.remove("Rectangle.json")
         except IOError:
@@ -404,11 +417,15 @@ class TestBase_load_from_file(unittest.TestCase):
 
 
 class TestBase_save_to_file_csv(unittest.TestCase):
-    """Unittests for testing save_to_file_csv method of Base class."""
+    """
+        Tests for testing save_to_file_csv method of Base class.
+    """
 
     @classmethod
     def tearDown(self):
-        """Delete any created files."""
+        """
+            Delete any created files.
+        """
         try:
             os.remove("Rectangle.csv")
         except IOError:
@@ -482,11 +499,15 @@ class TestBase_save_to_file_csv(unittest.TestCase):
 
 
 class TestBase_load_from_file_csv(unittest.TestCase):
-    """Unittests for testing load_from_file_csv method of Base class."""
+    """
+        Unittests for testing load_from_file_csv method of Base class.
+    """
 
     @classmethod
     def tearDown(self):
-        """Delete any created files."""
+        """
+            Delete any created files.
+        """
         try:
             os.remove("Rectangle.csv")
         except IOError:
