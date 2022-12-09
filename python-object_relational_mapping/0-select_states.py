e50a179ce5d1f96e982d86a4c316d2abdfbdfd44
YvesISH <y.ishimwe2@alustudent.com>
2022-12-09 05:07:12 -0800

    List all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""List all states from the databse."""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    mylist = cursor.fetchall()
+    for i in mylist:
+        print(i)
+    cursor.close()
+    db.close()
