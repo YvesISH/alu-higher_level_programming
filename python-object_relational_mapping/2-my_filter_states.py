fa00de0e9e70d358285784bbd6e87007f67996ef
YvesISH <y.ishimwe2@alustudent.com>
2022-12-09 05:49:20 -0800

    Filter states by user input.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that takes in arguments."""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY \
+            id ASC".format(argv[4]))
+    mylist = cursor.fetchall()
+    for i in mylist:
+        if i[4] == argv[4]:
+            print(i)
+    cursor.close()
+    db.close()
