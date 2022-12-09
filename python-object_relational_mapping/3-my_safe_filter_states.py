fd7e5e5bc043631f4db518621a815bb9dcebe797
YvesISH <y.ishimwe2@alustudent.com>
2022-12-09 06:10:59 -0800

    SQL Injection.

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                    (argv[4],))
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", 
+                   (argv[4],))
     myli = cursor.fetchall()
     for i in myli:
         print(i)
