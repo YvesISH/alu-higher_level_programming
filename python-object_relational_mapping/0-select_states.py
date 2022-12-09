204eb37b8a56804959eba297bd574e4637a89723
YvesISH <y.ishimwe2@alustudent.com>
2022-12-09 05:09:28 -0800

    List all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     mylist = cursor.fetchall()
