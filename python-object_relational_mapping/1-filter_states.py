cabcadd86d51c42e9602626e22531bcc17f6bcf4
YvesISH <y.ishimwe2@alustudent.com>
2022-12-09 05:19:11 -0800

    Filter states.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all states from the db starting with N."""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    mylist = cursor.fetchall()
+    for i in mylist:
+        print(i)
+    cursor.close()
+    db.close()
