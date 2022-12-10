199a5585598172f2ace74c3a0ad3831f98f4129b
YvesISH <y.ishimwe2@alustudent.com>
2022-12-10 13:27:38 -0800

    All cities by state.

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT cities.name FROM cities \
-            JOIN states ON cities.states_id = states.id WHERE states.name LIKE %s \
-            ORDER BY cities.id", (argv[4],))
+            JOIN states ON cities.states_id = states.id WHERE states.name \
+            LIKE %s ORDER BY cities.id", (argv[4],))
     mylist = cursor.fetchall()
     print(", ".join(city[0] for city in mylist))
     cursor.close()
