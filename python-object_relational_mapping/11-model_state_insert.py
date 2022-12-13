95ded79c4213c2066f3ca047fc46c0c6abe14294
YvesISH <y.ishimwe2@alustudent.com>
2022-12-13 01:38:31 -0800

    Add a new state.

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Add Louisiana to a DB."""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadat.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(str(state.id))
+    session.commit()
+    session.close()
