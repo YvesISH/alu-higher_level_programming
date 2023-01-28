29eb490c21bb51382e3d7ca531d14c2fa3e5e35c
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 09:41:18 -0800

    Second Biggest

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const secondBiggest = () => {
+  const args = process.argv.slice(2);
+  if (args.length < 2) {
+    console.log(0);
+    return;
+  }
+  const max = Number.MIN_SAFE_INTEGER;
+  const secondMax = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < args.length; i++) {
+    if (Number(args[i]) > max) {
+      secondMax = max;
+      max = Number(args[i]);
+    } else if (Number(args[i]) > secondMax) {
+      secondMax = Number(args[i]);
+    }
+  }
+  console.log(secondMax);
+};
