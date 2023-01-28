3f83ad7c098814597e0f8f6ea857df12d759c459
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 03:18:19 -0800

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args.length === 0) {
+	console.log('No argument');
+} else if (args.length === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
