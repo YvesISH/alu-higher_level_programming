ea3fee33629ba0a26c7639e892a566d3bae33231
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 08:38:09 -0800

    I love c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let x = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
-      console.log('C is fun');
+    console.log('C is fun');
   }
 }
