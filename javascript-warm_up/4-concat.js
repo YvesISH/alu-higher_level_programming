91108ec65cfc1189cf87dfb2a120168f0f24c85f
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 03:59:10 -0800

    Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 2) {
-  console.log(args[0] + 'is' + args[1]);
-} else {
-  console.log('please provide exactly 2 arguments');
-}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(arg1 + 'is' + arg2);
