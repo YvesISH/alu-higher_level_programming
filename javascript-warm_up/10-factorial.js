9225502a6fea14af984d39ea010dc9b0e3be5332
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 09:25:40 -0800

    Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (isNaN(n) || n < 0) {
-    return 1;
-  }
-  if ( n === 1 || n === 0) {
-    return 1;
-  }
+  if (isNaN(n) || n < 0) return 1;
+  if ( n === 1 || n === 0) return 1;
   return n * factorial(n - 1);
 }
 
