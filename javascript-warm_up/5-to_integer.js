a768f032b054d90483c9ec4414f72ada5f6c2f31
YvesISH <y.ishimwe2@alustudent.com>
2023-01-28 04:34:51 -0800

    An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const input = process.argv[2];
-const isNumber = !isNan(parseInt(input));
+const isNumber = !isNaN(parseInt(input));
 
 if (isNumber) {
   console.log(`My number: ${parseInt(input)}`);
