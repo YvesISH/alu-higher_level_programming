ef284c0694a06c69f021bfce91e10d3db8c4d462
YvesISH <y.ishimwe2@alustudent.com>
2023-01-30 00:53:16 -0800

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) return null;
       this.width = w;
       this.height = h;
-    }
   }
-};
+}
+
+module.exports = Rectangle;
