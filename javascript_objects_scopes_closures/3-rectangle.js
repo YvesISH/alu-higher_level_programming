12d36514b01a8816adac12a6bcf1234f55599628
YvesISH <y.ishimwe2@alustudent.com>
2023-01-30 01:43:24 -0800

    Rectangle #3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    const width = 'X'.repeat(this.width);
+    for (let i = 0; i <= this.height; i++) {
+      console.log(width);
+    }
+  }
+}
+
+module.exports = Rectangle;
