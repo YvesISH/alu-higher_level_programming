ebaf7cc5f6f0eaa28c4197c07755331eb5211e0f
YvesISH <y.ishimwe2@alustudent.com>
2023-02-06 04:27:28 -0800

    Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request.get(url, (error, result) => {
-  error ? console.log(err) : console.log(JSON.parse(result.body).title);
+  error ? console.log(error) : console.log(JSON.parse(result.body).title);
 });
